2ea4933ada433a818c1b95c9ee718304e2ed02b6
SamaraCardoso27 <samaracardosodossantos@gmail.com>
2015-03-24 08:09:47 -0300

    Preparando aplicação para deploy

--- backend/appengine/routes/admin/home.py
+++ backend/appengine/routes/admin/home.py
@@ -12,8 +12,7 @@ from routes.permission import home as permission_home, admin
 @permissions(ADMIN)
 @no_csrf
 def index():
-    return TemplateResponse()
-    #return TemplateResponse({'security_table_path': router.to_path(permission_home.index),
-    #                         'permission_admin_path': router.to_path(admin),
-    #                         'passwordless_admin_path': router.to_path(passwordless.form),
-    #                         'facebook_admin_path': router.to_path(facebook.form)})
+    return TemplateResponse({'security_table_path': router.to_path(permission_home.index),
+                             'permission_admin_path': router.to_path(admin),
+                             'passwordless_admin_path': router.to_path(passwordless.form),
+                             'facebook_admin_path': router.to_path(facebook.form)})
